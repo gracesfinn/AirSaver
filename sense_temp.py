05daa4099269a74dda6cdba892a1589fa492cb71
gracesfinn <graciefinn06@gmail.com>
2019-12-07 19:20:10 -0600

    New Temp - avg 38.79 AC ON

--- sense_temp.py
+++ sense_temp.py
@@ -2,11 +2,11 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 sense.clear()
-green = (0, 255, 0)
+blue = (0, 255, 0)
 red = (0,0,255)
 while True:
   temp = sense.get_temperature()
-  if temp >=25:
+  if temp >=39:
     sense.show_message("AC OFF!", text_colour = red)
   else:
     sense.show_message("AC ON!", text_colour = blue)
