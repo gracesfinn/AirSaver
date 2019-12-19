a0c5e1bba87d578965df3348d882f764ac1d9af5
gracesfinn <graciefinn06@gmail.com>
2019-12-19 13:48:37 -0600

    File clean up

--- index.js
+++ index.js
@@ -19,34 +19,25 @@ sense.Leds.clear();
 var wia = require('wia')('d_sk_VLdArRU3IwmPa6o6eJp4yTfV');
 
 // v1 write call back
-v1.on('write', function(param) {
-     var colour = param.map(Number);
-     sense.Leds.clear(colour);
-});
+
 
 v2.on('read', function() {
   IMU.getValue(function (e, data) {
      v2.write(data.temperature);
   })
 });
+
+
 v3.on('write', function(param) {
-  //has the button been pressed??
-	var button = 
-    console.log("It's a bit dark");
-    dark=true;
-    //You could do something interesting here like turn on lights!!!!
-  }
-  else
-  {
-   dark=false;
-  }
+  console.log('V3:', param[0]);
+  if (param[0]==1){
+        sense.Leds.clear(white)
+	//this button being pressed will tell the pi to switch on the AC unit
+    }else{
+        sense.Leds.clear();
+    }
 });
-v4.on('write', function(param) {
- console.log("v4: lat. " + param[0])
- wia.locations.publish({
-  latitude: param[0],
-  longitude: param[1]
-});
-});
+
+
 
 wia.stream.connect();
---
+++ presence_temp.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python coding=utf-8
+
+import subprocess
+
+import logging
+
+import urllib2
+import  json
+import  time
+
+logging.basicConfig(filename='/home/pi/AirSaver/presence-detector.log',level=logging.INFO, format='%(asctime)s - %(message)s')
+logging.info('Starting presence detector')
+
+from sense_hat import SenseHat
+
+from time import sleep
+
+WRITE_API_KEY='F5GSNHD01VIEIAU3'
+
+baseURL='https://api.thingspeak.com/update?api_key=%s' % WRITE_API_KEY
+
+
+sense = SenseHat()
+
+#Names of device owners
+names = ["Grace"]
+
+#MAC address of device
+macs = ["24:f0:94:d1:9a:e1"]
+
+#Temperature from SenseHat
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+blue = (0,128,128)
+
+
+#Code which combines presence detection and temperature sensors 
+#Has the AC been left on?
+def arp_scan():
+        try:
+                output = subprocess.check_output("sudo arp-scan -l", shell=True)
+                for i in range(len(names)):
+                        result = names[i]
+			temp = sense.get_temperature()
+                        if (macs[i] in output and temp <=39):
+                                result=result+" is home and AC is on"
+				tsFlag = 0
+                        else:
+                                result=result+" is not home - Auto AC shutdown"
+				tsFlag = 1
+                        print(result)
+                        sense.show_message(result, text_colour = blue)
+			return tsFlag
+        except Exception as e:
+                logging.error(e)
+
+
+#ThinkSpeak Code - needs work!
+def writeData(tsFlag):
+    # Sending the data to thingspeak in the query string
+    conn = urllib2.urlopen(baseURL + '&field1=%s' % (tsFlag))
+    print(conn.read())
+    # Closing the connection
+    conn.close()
+
+
+while True:
+	tsFlag=arp_scan()
+	writeData(tsFlag)
+        time.sleep(30)
+
+
+
+
